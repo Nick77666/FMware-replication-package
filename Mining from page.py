cb42fd9e289095cf5b72019bf6786ca60398c921
Nick77666 <91249686+Nick77666@users.noreply.github.com>
2024-03-09 13:14:06 -0500

    Delete Mining from page.py

--- Mining from page.py
+++
@@ -1,7 +0,0 @@
-import undetected_chromedriver as uc
-import time
-if __name__ == '__main__':
-    driver = uc.Chrome(headless=True,use_subprocess=False)
-    driver.get('https://nowsecure.nl')
-    time.sleep(2)
-    driver.save_screenshot('nowsecure.png')
